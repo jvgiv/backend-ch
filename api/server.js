58f8df8d820a8043e1c1c81d6134079d53aebee9
jvgiv <jvgiv12@gmail.com>
2021-01-05 16:51:30 -0500

    Basic server setup complete

---
+++ api/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const helmet = require('helmet')
+const cors = require('cors')
+
+
+const server = express()
+
+
+
+
+server.use(helmet())
+server.use(express.json())
+server.use(cors(
+    corsOptions
+))
+
+server.get('/', ,(req, res) => {
+    res.send(`Server's Working!`)
+})
